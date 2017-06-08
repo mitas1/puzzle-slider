9214d3d5debfa04f0f347906a885160c3c9617d0
filipKov <filo.kovac@gmail.com>
2017-06-08 22:06:01 +0200

    move css file, add constants for external resources

---
+++ PuzzleSlider/src/Global/ExternalResources.java
@@ -0,0 +1,11 @@
+package Global;
+
+public class ExternalResources {
+
+	public static final String PATH_FONT_FANCY = "/resources/fonts/PermanentMarker.ttf";
+	
+	public static final String PATH_CSS_FILE = "/resources/css/GUI_styles.css";
+
+	public static final String PATH_IMAGE_FOLDER = "/resources/images/";
+	
+}
